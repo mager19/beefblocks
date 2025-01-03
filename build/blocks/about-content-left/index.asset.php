fa5dfc9c4085e7d5530d172c710a0306a83b15e6
mager19 <mager19@gmail.com>
2025-01-03 18:51:37 -0500

    remove about contentleft

--- build/blocks/about-content-left/index.asset.php
+++ build/blocks/about-content-left/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '58e8fd9315dda400fe98');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '858a58e23767c08e3115');
